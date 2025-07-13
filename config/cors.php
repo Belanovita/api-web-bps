603690c8805b9ddc992d383c7abdf9036afeefa9
Belanovita <222212534@stis.ac.id>
2025-07-13 17:36:52 +0700

    cors lagi hehe

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://aplikasi-web-bps-five.vercel.app/'],
+    'allowed_origins' => ['http://localhost:5173', 'https://aplikasi-web-bps-five.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
